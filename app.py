3a579490ab433dc4dc764129c5f31ac5ca70fc51
rohithdepa <rohithreddydepa>
2022-06-16 16:49:51 +0530

    ussed json file

--- app.py
+++ app.py
@@ -1,69 +1,54 @@
+import json
 from flask import Flask, request, jsonify
-from sklearn.feature_extraction.text import CountVectorizer
-import Title ,TitleBody ,Body
 
 app = Flask(__name__)
-titleData=Title.titleModule()
-bodyData=Body.bodyModule()
-titleBodyData=TitleBody.bodyAndTitleModule()
+data=json.load(open('data.json'))
 @app.route('/')
 def index():
-  return jsonify('Working')
+  return jsonify('Home url is Working')
 @app.route("/metrics", methods=["GET", "POST"])
-def home():
+def metrics():
     args = request.args
     model=args.get('model')
-    if model=='Title':
-        data = titleData
-    elif model=='Body':
-        data = bodyData
-    elif model=="TitleBody":
-        data = titleBodyData
+    if(model in data):
+        res={'labels':data.get('labels'),'values':data.get(model)}
     else:
-        data={'error':'Wrong Parameter'}
-    return jsonify(data)
+        res={'error':'Wrong Parameter'}
+    return jsonify(res)
 @app.route("/predict",methods=["GET","POST"])
 def predict():
-    finaltags = []
-    args = request.args
-    line = args.get('data')
-    line = line.strip('\n')
-    line = line.strip()
-    line = line.lower()
-    if len(line) > 0:
-        cv1 = CountVectorizer(vocabulary=TitleBody.cv.get_feature_names(), stop_words="english", lowercase=True,
-                              ngram_range=(1, 2))
-        test1 = cv1.fit_transform([line])
-        predict = TitleBody.cls.predict(test1.toarray())[0]
+    # finaltags = []
+    # args = request.args
+    # line = args.get('data')
+    # line = line.strip('\n')
+    # line = line.strip()
+    # line = line.lower()
+    # if len(line) > 0:
+    #     cv1 = CountVectorizer(vocabulary=TitleBody.cv.get_feature_names(), stop_words="english", lowercase=True,
+    #                           ngram_range=(1, 2))
+    #     test1 = cv1.fit_transform([line])
+    #     predict = TitleBody.cls.predict(test1.toarray())[0]
 
-        for i in range(len(predict)):
-            if predict[i] == 1:
-                finaltags.append(Title.tag_names[i])
+    #     for i in range(len(predict)):
+    #         if predict[i] == 1:
+    #             finaltags.append(Title.tag_names[i])
 
-        finaltags = finaltags[0];
-    return jsonify(finaltags)
+    #     finaltags = finaltags[0];
+    # return jsonify(finaltags)
+    return jsonify('predict is under develpment')
 @app.route("/graphs",methods=["GET","POST"])
 def graphs():
-    data={}
-    data['labels']=['Title','Body','TitleBody'];
-    data['datasets'] =[]
     args = request.args
     model = args.get('type')
-    tmp = {}
-    if model == 'F1':
-        tmp['label']='F1 Graph';
-        tmp['backgroundColor']='#42A5F5';
-        tmp['data']=[titleData.get('values')[2],bodyData.get('values')[2],titleBodyData.get('values')[2]];
-    elif model == 'Precision':
-        tmp['label']='Precision Graph';
-        tmp['backgroundColor']='#FFE0B2';
-        tmp['data']=[titleData.get('values')[0],bodyData.get('values')[0],titleBodyData.get('values')[0]];
-    elif model == "Recall":
-        tmp['label'] = 'Recall Graph';
-        tmp['backgroundColor'] = '#42A5F5';
-        tmp['data'] = [titleData.get('values')[1], bodyData.get('values')[1], titleBodyData.get('values')[1]];
-    data['datasets'].append(tmp);
-
-    return jsonify(data)
+    final={}
+    res={}
+    final['labels']=['Title','Body','TitleBody']
+    final['datasets']=[]
+    res['label']=model+" Graph"
+    res['backgroundColor']='#FFE0B2' if(model=='Precision') else '#42A5F5'
+    idx=data.get('labels').index(model)
+    res['data'] = [data.get('Title')[idx], data.get('Body')[idx], data.get('TitleBody')[idx]];
+    final['datasets'].append(res);
+    return jsonify(final)
 if __name__ == "__main__":
     app.run()
